07d89f77c378989096b438fb44d21d6537502c7e
Eddie Allard <eddie@theallardcompany.com>
2021-03-25 14:59:58 -0400

    centered text inside jumbotron

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,18 @@
 import React from "react";
+import { Route, Switch } from 'react-router-dom';
+import HomePage from "./home/HomePage";
+import BetSlip from "./modal/BetSlip";
+import NavBar from "../components/common/NavBar";
+
 
 function App() {
-  return (
-    <div classNameName="container-fluid">
-      
-    
+  return(
+    <div className="container">
+      <NavBar />
+      <HomePage />
+      <Switch>
+        <Route path="Betslip" component={BetSlip} />
+      </Switch>
     </div>
   );
 }
--- src/components/home/HomePage.js
+++ src/components/home/HomePage.js
@@ -1,19 +1,23 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import Divider from '@material-ui/core/Divider';
 
 
 function HomePage() {
     return (
         <>
     <div className="jumbotron">
-        <h1>Man.United VS Chelsea</h1>
-        <h2>To Win</h2>
+        <h1 className="text-center">Man.United VS Chelsea</h1>
+        <Divider variant="middle" />
+        <h2 className="text-center">To Win</h2>
+        <div className="container text-center">
         <Link to="Betslip" className="btn btn-primary btn-lg">
             Man.United
         </Link>
         <Link to="Betslip" className="btn btn-primary btn-lg">
             Chelsea
         </Link>
+        </div>
     </div>
     </>
     );
