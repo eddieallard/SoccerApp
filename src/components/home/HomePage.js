8188908e9809d093d24d03959b716c54dd7bf311
Eddie Allard <eddie@theallardcompany.com>
2021-03-25 15:20:23 -0400

    wireframe  mockup homepage completed

--- src/components/home/HomePage.js
+++ src/components/home/HomePage.js
@@ -6,16 +6,42 @@ import Divider from '@material-ui/core/Divider';
 function HomePage() {
     return (
         <>
-    <div className="jumbotron">
+    <div className="jumbotron mt-5">
         <h1 className="text-center">Man.United VS Chelsea</h1>
         <Divider variant="middle" />
-        <h2 className="text-center">To Win</h2>
+        <h2 className="text-left">To Win</h2>
         <div className="container text-center">
-        <Link to="Betslip" className="btn btn-primary btn-lg">
-            Man.United
+        <Link to="Betslip" className="btn btn-primary btn-lg mr-5">
+            Man.United - 1.2
         </Link>
         <Link to="Betslip" className="btn btn-primary btn-lg">
-            Chelsea
+            Chelsea - 2.2
+        </Link>
+        </div>
+    </div>
+    <div className="jumbotron mt-5">
+        <h1 className="text-center">Arsenal VS Sporting CP</h1>
+        <Divider variant="middle" />
+        <h2 className="text-left">To Win</h2>
+        <div className="container text-center">
+        <Link to="Betslip" className="btn btn-primary btn-lg mr-5">
+            Arsenal - 1.2
+        </Link>
+        <Link to="Betslip" className="btn btn-primary btn-lg">
+            Sporting CP - 3
+        </Link>
+        </div>
+    <Divider variant="middle" />
+        <h2 className="text-left">To Score First</h2>
+        <div className="container text-center">
+        <Link to="Betslip" className="btn btn-primary btn-lg mr-5">
+            Alexis - 3.1
+        </Link>
+        <Link to="Betslip" className="btn btn-primary btn-lg mr-5">
+            Giroud - 2.1
+        </Link>
+        <Link to="Betslip" className="btn btn-primary btn-lg">
+            Lacazette - 1.5
         </Link>
         </div>
     </div>
