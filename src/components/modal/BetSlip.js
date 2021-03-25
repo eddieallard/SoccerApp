e4da0e57e3d1ab0b59aee5a5d90d9d372af12428
Eddie Allard <eddie@theallardcompany.com>
2021-03-25 14:42:04 -0400

    created modal for Betslip

---
+++ src/components/modal/BetSlip.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+
+function BetSlip() {
+    return (
+        <div className="modal" tabindex="-1">
+  <div className="modal-dialog">
+    <div className="modal-content">
+      <div className="modal-header">
+        <h5 className="modal-title">Modal title</h5>
+        <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+      </div>
+      <div className="modal-body">
+        <p>Modal body text goes here.</p>
+      </div>
+    </div>
+  </div>
+</div>
+    );
+};
+
+export default BetSlip;
